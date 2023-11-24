464028d507b68a5489a7d4a35f7d9ae618fd02e4
HSSJW <132650844+HSSJW@users.noreply.github.com>
2023-11-24 20:42:21 +0900

    Update fib.py

--- fib.py
+++ fib.py
@@ -8,10 +8,10 @@ Negative numbers should return None
 def fibonacci(position):
      if position < 0:
           return None
-       elif position == 0:
-            return 0
-         elif position == 1:
-              return 1
-           else:
-                return fibonacci(position - 1) + fibonacci(position - 2)
+     elif position == 0:
+          return 0
+     elif position == 1:
+          return 1
+     else:
+          return fibonacci(position - 1) + fibonacci(position - 2)
 
